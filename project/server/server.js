28243cb847a8dac1a6e5d15d707f10db26281f26
Varsha_Sabu <stu.vs.pro@gmail.com>
2025-07-06 04:59:37 +0530

    dbchange

--- project/server/server.js
+++ project/server/server.js
@@ -103,7 +103,7 @@ const startServer = async () => {
   try {
     // Connect to MongoDB
     console.log('🔄 Connecting to MongoDB...');
-    await mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://stuvspro:%23CrackiT@cluster0.1acee.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {
+    await mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://stuvspro:%23CrackiT@cluster0.1acee.mongodb.net/myntra-clone?retryWrites=true&w=majority&appName=Cluster0", {
       retryWrites: true,
       w: 'majority'
     });
